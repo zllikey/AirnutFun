44cbf852eaff6ebda7eba47737fd1e4bb77549ed
zllikey@gmail.com <zllikey@gmail.com>
2021-10-13 22:27:16 +0800

    no message

--- custom_components/airnut/__init__.py
+++ custom_components/airnut/__init__.py
@@ -126,7 +126,7 @@ def get_time():
     return (datetime.datetime.utcnow() + datetime.timedelta(hours=8)).strftime("%Y-%m-%d %H:%M:%S")
 
 def get_time_unix():
-    return int((datetime.datetime.utcnow() + datetime.timedelta(hours=8)).timestamp())
+    return int((datetime.datetime.now() + datetime.timedelta(hours=8)).timestamp())
         
 async def async_setup_entry(hass: HomeAssistantType, entry: ConfigEntry):
     hass.async_create_task(
